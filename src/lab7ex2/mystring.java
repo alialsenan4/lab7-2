22e398e1c3d178d7f45ecd1096152cc57b67cd7b
2211130159 <2211130159@CPES-3B1037-P18.ku.edu.kw>
2025-11-11 09:25:19 +0300

    p

--- src/lab7ex2/mystring.java
+++ src/lab7ex2/mystring.java
@@ -19,7 +19,7 @@ public class mystring {
 			for ( i=0; i<n; i++) {
 			if (Temp[i] >= 'A' && Temp[i] <= 'Z')
 				Temp[i]= (char)(Temp[i] - 'A' + 'a') ;
-			//else
+			else
 			if (Temp[i] >= 'a' && Temp[i] <= 'z')
 				Temp[i]= (char)(Temp[i] - 'a' + 'A') ;
 			Text += Temp[i];
